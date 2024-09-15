7fd1da192c2d9c49e0fe1c7883a78e07248c1794
d2min <aamor8448@gmail.com>
2024-09-16 00:24:27 +0300

    Додеал стили для страницы создания карточки, осталось отправить данные на сервер

--- src/Components/Pages/CreateCard/CreateCard.tsx
+++ src/Components/Pages/CreateCard/CreateCard.tsx
@@ -6,6 +6,9 @@ import { InputForm } from "../../Shared/InputForm/InputForm";
 export function CreateCard(){
     
     const [selectedImage, setSelectedImage] = useState<string | null>(null);
+    const [messageStyle, setMessageStyle] = useState<string>('');
+    const [message, setMessage] = useState('')
+
     function handleImageChange(event:any){
         const file = event.target.files[0];
         if(file){
@@ -15,8 +18,9 @@ export function CreateCard(){
     
     return(
         <div className={style.CreateCard}>
+            <div className={`${style.submitMassege} ${style[messageStyle]}`}>{message}</div>
+            <h1 className={style.createTitle}>Создание карточки</h1>
             <div className={style.createCardSection}>
-                <h1 style={{margin:'10px'}}>Создание карточки</h1>
                 <div className={style.imgCard}>
                     <input
                         className={style.fileImg}
@@ -42,12 +46,37 @@ export function CreateCard(){
                     </div>
                 </div>
                 <div className={style.inputs}>
-                    <InputForm text='Sad' title="Слово"/>
-                    <InputForm text='Грустный' title="Перевод"/>
-                    <InputForm text='Описание большое' title="Описание"/>
+                    <InputForm text='' title="Слово"/>
+                    <InputForm text='' title="Перевод"/>
+                    <InputForm text='' title="Описание"/>
                 </div>
                 <div className={style.button}>
-                    <ButtonForm text="Создать"/>
+                    <ButtonForm setMessageStyle={setMessageStyle} setMessage={setMessage} message="Карточка создана" text="Создать"/>
+                </div>
+            </div>
+            <div className={style.helper}>
+                <h1>Инструкция по созданию карточки</h1>
+                <ul className={style.helperList}>
+                    <li>Добавте фотограцию для вашей карточки, это можно сделать нажав по кнопке <strong>Выбрать фото</strong> или перенести фоту в поле для фотографии</li>
+                    <li>Введите изучаемое слово в поле <strong>Слово</strong></li>
+                    <li>Введите перевод изуаемого слова в поле <strong>Перевод</strong></li>
+                    <li>По необходимости можете добавить краткое описание в поле <strong>Описание</strong>, оно может помоч вам в изучении слов</li>
+                </ul>
+                <div className={style.examples}>
+                    <h1>Пример заполнения #1</h1>
+                    <p>
+                        Мы хотим изучить слово <strong>Город</strong> на английском. Для этого ищем
+                        и скачиваем изображение города. После добавляем его в поле для фотографии. 
+                        В поле <strong>слово</strong> пишем <strong>Citi</strong>, а в поле <strong>перевод</strong> - <strong>Город</strong>. 
+                        Описание оставим пустым
+                    </p>
+                    <h1>Пример заполнения #2</h1>
+                    <p>
+                        Мы хотим изучить все то же слово <strong>Город</strong> на английском, но с другим подходом.
+                        Для этого ищем и скачиваем изображение рыбаловной сети. После добавляем его в поле для фотографии. 
+                        В поле <strong>слово</strong> пишем <strong>Citi</strong>, а в поле <strong>перевод</strong> - <strong>Город</strong>. 
+                        Но в описание добавляем ассоциативную подсказку: <strong>'Город, словно сеть связывает дома дорогами'</strong>
+                    </p>
                 </div>
             </div>
         </div>
